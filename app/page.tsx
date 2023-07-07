8de35ca135c79ec275aec1ede086cbdff80690a3
PGjun <pgj127@gmail.com>
2023-07-07 20:14:31 +0900

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,27 +2,21 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-between">
-      <Link href={"/notification"}>
-        <button className=" h-[40px] w-[150px] bg-[#7988B8] text-white font-bold rounded-lg">
-          긴급공지 관리
-        </button>
-      </Link>
-      <Link href={"/account"}>
-        <button className=" h-[40px] w-[150px] bg-[#7988B8] text-white font-bold rounded-lg">
-          계정 관리
-        </button>
-      </Link>
-      <Link href={"/emrdata"}>
-        <button className=" h-[40px] w-[150px] bg-[#7988B8] text-white font-bold rounded-lg">
-          데이터 관리
-        </button>
-      </Link>
-      <Link href={"/status"}>
-        <button className=" h-[40px] w-[150px] bg-[#7988B8] text-white font-bold rounded-lg">
-          처치 현황
-        </button>
-      </Link>
+    <main className="flex flex-col items-center min-h-screen text-[14px] font-bold">
+      <div className="flex flex-col items-center justify-between bg-white w-[330px] h-[150px] rounded-xl shadow-lg">
+        <div className="flex items-end text-[12px] gap-[30px] h-[50px] border mt-[20px]">
+          <div>액티비티 & 체험</div>
+          <div>호텔</div>
+          <div>교통</div>
+          <div>렌터카</div>
+        </div>
+
+        <Link href={"/notification"}>
+          <button className="h-[40px] w-[300px] bg-slate-100  rounded-xl mb-[15px]">
+            모든 카테고리
+          </button>
+        </Link>
+      </div>
     </main>
   );
 }
